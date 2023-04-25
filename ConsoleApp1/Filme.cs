4bf378cf590af87940d3db71df636909a8572557
Adilson Roberto Cavicchioli Junior <101985616+juniorcavicchioli@users.noreply.github.com>
2023-04-25 11:39:01 -0300

    Add files via upload

---
+++ ConsoleApp1/Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    internal class Filme
+    {
+        public string? Nome { get; set; }
+        public int Ano { get; set; }
+        public string? Genero { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using HtmlAgilityPack;
+
+Console.WriteLine("Iniciando robozinho!");
+var Urls = new string[]
+{
+    "https://www.rottentomatoes.com/m/the_super_mario_bros_movie",
+    "https://www.rottentomatoes.com/m/war_for_the_planet_of_the_apes"
+};
+
+foreach (var url in Urls)
+    RodarRobozinho(url);
+
+
+void RodarRobozinho(string url)
+{
+    var PageResult = new HttpClient().GetAsync(url).Result;
+
+
+
+    var Html = PageResult.Content.ReadAsStringAsync().Result;
+    var doc = new HtmlDocument();
+    doc.LoadHtml(Html);
+
+    Console.WriteLine(doc);
+
+
+    var NovoFilme = new Filme()
+    {
+        Nome = doc.DocumentNode.SelectSingleNode("//*[@id=\"topSection\"]/div[1]/score-board/h1").InnerText
+    };
+
+    Console.WriteLine(NovoFilme.Nome);
+}
